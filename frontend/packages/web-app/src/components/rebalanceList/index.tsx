5e6431092eb670c09902592aa2227fb2e0f65472
Ivan <ivan.alberquilla@gmail.com>
2023-05-31 11:20:05 +0200

    load strategy data

--- frontend/packages/web-app/src/components/rebalanceList/index.tsx
+++ frontend/packages/web-app/src/components/rebalanceList/index.tsx
@@ -1,13 +1,5 @@
-import { TransferListItem, CardText } from '@aragon/ui-components';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
-import {Dd, DescriptionListContainer, Dl, Dt} from 'components/descriptionList';
-
-
-import { trackEvent } from 'services/analytics';
-import { abbreviateTokenAmount } from 'utils/tokens';
-import { Transfer } from 'utils/types';
+import { Dd, DescriptionListContainer, Dl, Dt } from 'components/descriptionList';
 
 type TransferListProps = {
   rebalances: any[];
@@ -17,29 +9,29 @@ const RebalanceList: React.FC<TransferListProps> = ({
   rebalances
 }) => {
 
-  if (rebalances.length === 0)
+  if (rebalances?.length === 0 || !rebalances)
     return <p data-testid="transferList">{'No rebalances yey'}</p>;
 
   return (
     <div className="space-y-2" data-testid="transferList">
       {rebalances.map((rebalance, index) => (
         <>
-        <DescriptionListContainer
-          title={"Rebalance"}
-        >
-          <Dl>
-            <Dt>{"Executed"}</Dt>
-            <Dd>{"Yes"}</Dd>
-          </Dl>
-          <Dl>
-            <Dt>{"Allocation"}</Dt>
-            <Dd>{"BTC 0,2 LINK 0,2 USDC 0,6"}</Dd>
-          </Dl>
-          <Dl>
-            <Dt>{"Reasoning"}</Dt>
-            <Dd>{"Current allocation does not align with risk parity strategy"}</Dd>
-          </Dl>
-        </DescriptionListContainer>
+          <DescriptionListContainer
+            title={"Rebalance"}
+          >
+            <Dl>
+              <Dt>{"Executed"}</Dt>
+              <Dd>{rebalance.args.rebalanceRequired}</Dd>
+            </Dl>
+            <Dl>
+              <Dt>{"Allocation"}</Dt>
+              <Dd>{"BTC 0,2 LINK 0,2 USDC 0,6"}</Dd>
+            </Dl>
+            <Dl>
+              <Dt>{"Reasoning"}</Dt>
+              <Dd>{rebalance.args.reason}</Dd>
+            </Dl>
+          </DescriptionListContainer>
         </>
       ))}
     </div>
